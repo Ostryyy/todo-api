5666755cef8a3aae30b2f130e5bc77f07fd21fd9
Arkadiusz Ostrowski <arek.ostrowski135@gmail.com>
2024-07-26 22:17:34 +0200

    Add auth controller

---
+++ src/controllers/authController.js
@@ -0,0 +1,30 @@
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../config");
+
+exports.register = async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = new User({ username, password });
+    await user.save();
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    res.status(400).json({ error: "Username already exists" });
+  }
+};
+
+exports.login = async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await User.findOne({ username });
+    if (!user || !(await user.comparePassword(password))) {
+      return res.status(401).json({ error: "Invalid credentials" });
+    }
+    const token = jwt.sign({ userId: user._id }, JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    res.json({ token });
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
