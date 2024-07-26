3b8b49ed25a27498b77692604909534049b54174
Arkadiusz Ostrowski <arek.ostrowski135@gmail.com>
2024-07-26 22:21:06 +0200

    Add auth routes

--- src/server.js
+++ src/server.js
@@ -2,11 +2,16 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const { PORT, DB_URI } = require("./config");
+const authRoutes = require("./routes/auth");
+
 const app = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+// Routes
+app.use("/api/auth", authRoutes);
 
 // Connect to MongoDB and start the server
 mongoose
